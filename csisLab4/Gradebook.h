e5ce0c213d72282eaafe8dd1717f5c6d54d6a965
kulny <wordworthathousandpictures@gmail.com>
2018-10-03 13:58:29 -0400

    pseudo

--- csisLab4/Gradebook.h
+++ csisLab4/Gradebook.h
@@ -2,9 +2,13 @@
 class Gradebook
 {
 public:
-	Gradebook();
+	Gradebook(); // initializes to gradebook name
 	
 private:
+	// vector of students
+
+	// add another student to gradebook (student push_back)
+
 
 
 
--- csisLab4/Student.h
+++ csisLab4/Student.h
@@ -1,10 +1,19 @@
 #pragma once
+
+#include <array>
+
 class Student
 {
 public:
 	Student();
 
 private:
+	std::array<double, 5> grades;
+	std::string lName;
+	// input grades
+
+	// setters and getters fName, lName
+
 
 
 };
